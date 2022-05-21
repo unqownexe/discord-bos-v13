0f888d70b1426822dc0a92df66193d45948945db
Unqown Exe <76444282+unqownexe@users.noreply.github.com>
2022-05-21 07:48:13 +0300

    Add files via upload

---
+++ commands/Unqown.js
@@ -0,0 +1,10 @@
+const Discord = require("discord.js");
+module.exports.run = async (client, message, args) => {
+message.channel.send('ayn ondan ')
+};
+
+exports.config = {
+  name: "alo",
+  guildOnly: true,
+  aliases: [],
+};
